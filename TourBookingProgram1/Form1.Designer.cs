6dc4da90c12e55b88c2efbafe097c0d322ebdcff
Amy <hofman.amy@gmail.com>
2020-09-20 16:02:23 +1200

    First changes

---
+++ TourBookingProgram1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TourBookingProgram1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate tour cost";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Safari",
+            "Mountain",
+            "Tropical"});
+            this.comboBox1.Location = new System.Drawing.Point(145, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(145, 153);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(31, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 72);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(464, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Choose a type of tour";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "How many people?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Add this tour";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TourBookingProgram1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TourBookingProgram1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //string tourType = comboBox1.SelectedItem.ToString();
+            //decimal tourNumberOfPeople = numericUpDown1.Value;
+            //decimal tourCostPerPerson = TourCostPerPerson();
+        }
+
+        private string TourType()
+        {
+            string tourType = comboBox1.SelectedItem.ToString();
+            return tourType;
+        }
+
+        private decimal TourNumberOfPeople()
+        {
+            decimal tourNumberOfPeople = numericUpDown1.Value;
+            return tourNumberOfPeople;
+        }
+
+        private decimal TourCostPerPerson()
+        {
+            string tourType = comboBox1.SelectedItem.ToString();
+
+            if (tourType == "Safari")
+            {
+                return 400;
+            }
+            if (tourType == "Mountain")
+            {
+                return 100;
+            }
+            else //Tropical
+            {
+                return 300;
+            }
+        }
+
+        private string TourTotalCostString()
+        {
+            decimal tourTotalCost;
+            tourTotalCost = TourNumberOfPeople() * TourCostPerPerson();
+            string tourTotalCostString = tourTotalCost.ToString();
+            return tourTotalCostString;
+        }
+
+        private string TourDescriptionLine()
+        {
+            string Tour = "A " + TourType() + " tour for " + TourNumberOfPeople() + " will cost you " + TourTotalCostString() + ".\nTo add this to your booking click Add Booking.";
+
+            return Tour;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((comboBox1.SelectedItem == null) || (numericUpDown1.Value < 1))
+            {
+                MessageBox.Show("Please complete all fields.");
+            }
+            else
+            {
+                label1.Text = TourDescriptionLine();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(TourDescriptionLine());
+        }
+    }
+}
